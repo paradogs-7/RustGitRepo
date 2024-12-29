d238410d25674aa6e9688e32392803a4ed2184bf
Your Name <vnyz2002@gmail.com>
2024-12-29 19:05:03 +0300

    Add generated files to target/debug directory

---
+++ First/target/debug/First.d
@@ -0,0 +1,1 @@
+C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\target\debug\First.exe: C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\src\main.rs
---
+++ First/target/debug/deps/First-96ba24c989262628.d
@@ -0,0 +1,5 @@
+C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\target\debug\deps\First-96ba24c989262628.exe: src/main.rs
+
+C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\target\debug\deps\First-96ba24c989262628.d: src/main.rs
+
+src/main.rs:
