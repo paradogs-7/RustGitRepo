e157a0cd2b05e41282cb7374cc732c7cd9303aef
Your Name <vnyz2002@gmail.com>
2024-12-29 19:04:50 +0300

    Remove workspace.xml and update main.rs with variable shadowing examples

---
+++ First/target/debug/deps/First-7cd2f81fa0003253.d
@@ -0,0 +1,5 @@
+C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\target\debug\deps\libFirst-7cd2f81fa0003253.rmeta: src/main.rs
+
+C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\target\debug\deps\First-7cd2f81fa0003253.d: src/main.rs
+
+src/main.rs:
---
+++ First/target/debug/deps/First-fe8eb678b3e93161.d
@@ -0,0 +1,5 @@
+C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\target\debug\deps\libFirst-fe8eb678b3e93161.rmeta: src/main.rs
+
+C:\Users\Volkan\OneDrive\Belgeler\GitHub\RustGitRepo\First\target\debug\deps\First-fe8eb678b3e93161.d: src/main.rs
+
+src/main.rs:
